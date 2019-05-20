9613fab9f1d8e14761d410af31c1a1044bb1953d
nyoronyoro111 <yuma.maron_1026@icloud.com>
2019-05-20 13:10:30 +0900

    0~999までできた？

--- src/intNumToStringNum/IntToEng.java
+++ src/intNumToStringNum/IntToEng.java
@@ -3,10 +3,11 @@ package intNumToStringNum;
 import java.util.Scanner;
 
 public class IntToEng {
-	String[] OneToTen = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
-	String[] ElevenToNineteen = {};
-	String[] digit= {"Hundred"};
-	//
+	static String[] OneToTen = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","ten"};
+	static String[] TenToNineteen = {"Ten","Eleven","Twenteen","Thirteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+	static String[] Ten_digit={"Twenty","Thirty","Fourtey","Fifty","Sixty","Seventy","Eighty","Ninety"};
+	static String[] digit= {"Thousand","Million","Billion"};
+
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
@@ -15,16 +16,27 @@ public class IntToEng {
 		System.out.println(translateEng(input));
 
 	}
+	public static String translateEng(int n,String str) {
+		int i=n;
+
+		if(i==100) {
+			str=str+" hundred";
+		}else if(i>100)str=str+OneToTen[(i/100)-1]+" hundreds";
+		int k=(i/10)%10;
+		if(k==1) {//10~19のとき
+			str=str+" "+TenToNineteen[(i%10)-1];
+		}else if(k>0) {//20~99のとき
+			str=str+" "+Ten_digit[k-2];
+			if(i%10!=0) {str=str+" "+OneToTen[(i%10)-1];}
+		}else {//0~9のとき、ただし0は何も表示しない。
+			if(i%10!=0) {str=str+" "+OneToTen[(i%10)-1];}
+		}
+
+		return str;
+	}
 	public static String translateEng(int n) {
 		if(n==0)return "Zero";
-
-
-		/*
-		for(int i=1000000000;i>0;i=i/100) {
-		}
-		*/
-		return "";
+		return translateEng(n,"");
 	}
-
 
 }
