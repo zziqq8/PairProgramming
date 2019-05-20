5291c6c04a1674d599fad84f19302238ef1817b5
kanayanagisawa <kanayanagisawa@172.19.12.9>
2019-05-20 13:43:37 +0900

    testに桁数の多いものを追加しました。

--- src/intNumToStringNum/IntToEng.java
+++ src/intNumToStringNum/IntToEng.java
@@ -3,25 +3,28 @@ package intNumToStringNum;
 import java.util.Scanner;
 
 public class IntToEng {
-	static String[] OneToTen = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","ten"};
-	static String[] TenToNineteen = {"Ten","Eleven","Twenteen","Thirteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
-	static String[] Ten_digit={"Twenty","Thirty","Fourtey","Fifty","Sixty","Seventy","Eighty","Ninety"};
+	static String[] OneToTen = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
+	static String[] TenToNineteen = {"Ten","Eleven","Twelve","Thirteen","Forteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+	static String[] Ten_digit={"Twenty","Thirty","Fourty","Fifty","Sixty","Seventy","Eighty","Ninety"};
 	static String[] digit= {"Thousand","Million","Billion"};
 
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("英語に変換したい数字を入力してください.");
 		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt();
-		System.out.println(translateEng(input));
-
+		int input = 0;
+		while(sc.hasNext()) {
+			input = sc.nextInt();
+			System.out.println(translateEng(input));
+		}
 	}
 	public static String translateEng(int n,String str) {
 		int i=n;
 
-		if(i==100) {
-			str=str+" hundred";
-		}else if(i>100)str=str+OneToTen[(i/100)-1]+" hundreds";
+		if(i%10==1) {
+			str="One Hundred";
+		}else if(i>200)str=str+OneToTen[(i/100)-1]+" Hundreds";
 		int k=(i/10)%10;
 		if(k==1) {//10~19のとき
 			str=str+" "+TenToNineteen[(i%10)-1];
